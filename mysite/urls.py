f9678be4aa9765fb578c2cb26770824da5b084ff
le1zyyx <le1zyyx@gmail.com>
2025-11-11 22:57:46 +0200

    Add OpenAPI (Swagger)

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from main import views as main_views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="E-Queue API",
+      default_version='v1',
+      description="API documentation for E-Queue",
+      contact=openapi.Contact(email="your@email.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', main_views.home, name='home'),
@@ -11,6 +25,7 @@ urlpatterns = [
     path('login/', main_views.login_page, name='login_page'),
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
